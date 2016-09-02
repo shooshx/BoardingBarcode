78ffe62f725b5440397a54dae10a35c2afa0de5c
Shy Shalom <shooshx@gmail.com>
2016-09-02 20:03:25 +0300

    build in release

---
+++ em_rel_build.bat
@@ -0,0 +1,5 @@
+rem %EMSCRIPTEN%\emcc --clear-cache
+rem --tracing
+rem -D_LIBCPP_DEBUG=0 -x c++ -std=c++11
+
+%EMSCRIPTEN%\emcc -O3  -s -s ALLOW_MEMORY_GROWTH=0 -D_DEBUG -D_LIBCPP_DEBUG=0 --memory-init-file 0 src/aztec.c src/common.c src/library.c src/reedsol.c src/render.c src/svg.c  src/main.cpp -Isrc -Wno-switch -o js_qr_brd.html -s EXPORTED_FUNCTIONS="['_run', '_getDayOfYear']"
